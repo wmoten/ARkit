0e84e9c707bb1b834c4d118c214d182dd72a6198
Will <wmote20@gmail.com>
2021-02-21 11:40:20 -0500

    Initial Commit

---
+++ ARF/ARFApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFApp.swift
+//  ARF
+//
+//  Created by William Moten on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct ARFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
